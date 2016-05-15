65f661c052cee1b6f54b06de1b44245d264a9750
hbaderman <hillel@baderman.org.uk>
2016-05-15 17:34:34 +0100

    finished insertion sort

--- insertionsort.py
+++ insertionsort.py
@@ -9,10 +9,12 @@ def insert(arr, rightIndex, value):
 	return arr
 
 def insertionSort(arr):
+	for i in range(0,len(arr)-1):
+		arr = insert(arr,i,arr[i+1])
+	return arr
 
 
-
-
+# INSERT TEST CASES
 array = [3, 5, 7, 11, 13, 2, 9, 6]
 test1 = insert(array, 4, 2)
 assert(test1 == [2, 3, 5, 7, 11, 13, 9, 6])
@@ -20,3 +22,8 @@ test2 = insert(array, 5, 9)
 assert(test2 == [2, 3, 5, 7, 9, 11, 13, 6])
 test3 = insert(array, 6, 6)
 assert(test3 == [2, 3, 5, 6, 7, 9, 11, 13])
+
+# INSERTIONSORT TEST CASES
+array = [22, 11, 99, 88, 9, 7, 42]
+test1 = insertionSort(array)
+assert(test1 == [7, 9, 11, 22, 42, 88, 99])
